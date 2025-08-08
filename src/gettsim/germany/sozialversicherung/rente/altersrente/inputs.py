0cfb84026cde02fd5c6d8ab836213ea546333386
Marvin Immesberger <immesberger@posteo.de>
2025-08-08 19:34:24 +0200

    Remove some orphaned nodes.

--- src/gettsim/germany/sozialversicherung/rente/altersrente/inputs.py
+++ src/gettsim/germany/sozialversicherung/rente/altersrente/inputs.py
@@ -5,7 +5,7 @@ from __future__ import annotations
 from gettsim.tt import policy_input
 
 
-@policy_input()
+@policy_input(end_date="2022-12-31")
 def höchster_bruttolohn_letzte_15_jahre_vor_rente_y() -> float:
     """Highest gross income from regular employment in the last 15 years before pension
     benefit claiming. Relevant to determine pension benefit deductions for retirees in
