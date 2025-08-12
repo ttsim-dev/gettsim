e7ca92a67ecf490e2d01c8bc093565ab1b1bbe7f
Juergen Wiemers <juergen.wiemers@gmail.com>
2025-08-12 14:08:52 +0200

    Optimize `in_anderer_bg_als_kindergeldempfänger`

--- src/gettsim/germany/bürgergeld/kindergeldübertrag.py
+++ src/gettsim/germany/bürgergeld/kindergeldübertrag.py
@@ -119,17 +119,15 @@ def in_anderer_bg_als_kindergeldempfänger(
     """True if the person is in a different Bedarfsgemeinschaft than the
     Kindergeldempfänger of that person.
     """
-    # Get the array index for all p_ids of empfängers
-    p_id_empfänger_loc = kindergeld__p_id_empfänger
-    for i in range(p_id.shape[0]):
-        p_id_empfänger_loc = xnp.where(
-            kindergeld__p_id_empfänger == p_id[i],
-            i,
-            p_id_empfänger_loc,
-        )
-
+    # Create a mask for the kindergeld__p_id_empfänger
     # Map each kindergeld__p_id_empfänger to its corresponding bg_id
-    empf_bg_id = bg_id[p_id_empfänger_loc]
+    empf_bg_id = join(
+        foreign_key=kindergeld__p_id_empfänger,
+        primary_key=p_id,
+        target=bg_id,
+        value_if_foreign_key_is_missing=-1,
+        xnp=xnp,
+    )
 
     # Compare bg_id array with the mapped bg_ids of kindergeld__p_id_empfänger
     return empf_bg_id != bg_id
