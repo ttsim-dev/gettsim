bf9968ddb04ad5fdc7f14fe6527db33fdd6bf152
Hans-Martin von Gaudecker <hmgaudecker@gmail.com>
2025-08-22 14:45:00 +0200

    Fix handling of capital income, fixes #843.
    In the process,
    
    - Use 'werbungskostenpauschbetrag' instead of 'sparer_werbungskostenpauschbetrag'
      (thanks, namespaces)
    - Keep the calculation of Einkünfte aus Kapitalvermögen in the
      ('einkommensteuer', 'einkünfte', 'aus_kapitalvermögen') namespace throughout

--- src/gettsim/germany/einkommensteuer/abgeltungssteuer/abgeltungssteuer.py
+++ src/gettsim/germany/einkommensteuer/abgeltungssteuer/abgeltungssteuer.py
@@ -6,27 +6,8 @@ from gettsim.tt import policy_function
 
 
 @policy_function(start_date="2009-01-01")
-def betrag_y_sn(zu_versteuernde_kapitalerträge_y_sn: float, satz: float) -> float:
-    """Abgeltungssteuer on Steuernummer level."""
-    return satz * zu_versteuernde_kapitalerträge_y_sn
-
-
-@policy_function(start_date="2009-01-01")
-def zu_versteuernde_kapitalerträge_y_sn(
-    familie__anzahl_personen_sn: int,
-    einnahmen__kapitalerträge_y_sn: float,
-    einkommensteuer__einkünfte__aus_kapitalvermögen__sparerpauschbetrag: float,
+def betrag_y_sn(
+    einkommensteuer__einkünfte__aus_kapitalvermögen__betrag_y_sn: float, satz: float
 ) -> float:
-    """Taxable capital income for Abgeltungssteuer.
-
-    TODO(@MImmesberger): Find out whether Sparerpauschbetrag is
-    transferable to partner with same sn_id.
-    https://github.com/ttsim-dev/gettsim/issues/843
-
-    """
-    out = (
-        einnahmen__kapitalerträge_y_sn
-        - familie__anzahl_personen_sn
-        * einkommensteuer__einkünfte__aus_kapitalvermögen__sparerpauschbetrag
-    )
-    return max(out, 0.0)
+    """Abgeltungssteuer on Steuernummer level."""
+    return satz * einkommensteuer__einkünfte__aus_kapitalvermögen__betrag_y_sn
