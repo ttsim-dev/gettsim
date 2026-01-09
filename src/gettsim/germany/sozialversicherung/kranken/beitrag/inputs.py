86c636e0c7ed5f095dc5eff159cbe87d36b9c38d
Marvin Immesberger <74215010+MImmesberger@users.noreply.github.com>
2026-01-09 12:17:01 +0100

    Vorsorgepauschale Reform 2026 (#1113)
    ### What problem do you want to solve?
    
    #1098 
    
    Also closes #1116
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- src/gettsim/germany/sozialversicherung/kranken/beitrag/inputs.py
+++ src/gettsim/germany/sozialversicherung/kranken/beitrag/inputs.py
@@ -8,3 +8,9 @@ from gettsim.tt import policy_input
 @policy_input()
 def privat_versichert() -> bool:
     """Has (only) a private health insurance contract."""
+
+
+@policy_input()
+def beitrag_private_basiskrankenversicherung_abzüglich_arbeitgeberanteil_m() -> float:
+    """Monthly contribution to private basic health insurance minus (tax-exempt)
+    employer's contribution."""
