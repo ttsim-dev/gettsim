93046ecb3035e9b827e7f62145c58d7d2e8ece2b
Marvin Immesberger <immesberger@posteo.de>
2026-05-21 14:50:02 +0200

    Review.

--- src/gettsim/germany/einkommensteuer/einkünfte/sonstige/sonstige.py
+++ src/gettsim/germany/einkommensteuer/einkünfte/sonstige/sonstige.py
@@ -7,28 +7,14 @@ from gettsim.tt import policy_function
 
 @policy_function()
 def betrag_y(
-    einnahmen_y: float,
-    werbungskosten_y: float,
-) -> float:
-    """Sonstige Einkünfte nach Abzug der Werbungskosten."""
-    return max(einnahmen_y - werbungskosten_y, 0.0)
-
-
-@policy_function()
-def einnahmen_y(
-    alle_weiteren_y: float,
     rente__steuerpflichtige_einnahmen_y: float,
-) -> float:
-    """Steuerpflichtige Einnahmen aus sonstigen Einkünften i.S.d. § 22 EStG."""
-    return alle_weiteren_y + rente__steuerpflichtige_einnahmen_y
-
-
-@policy_function()
-def werbungskosten_y(
+    alle_weiteren_y: float,
     werbungskostenpauschbetrag: float,
 ) -> float:
-    """Werbungskosten für sonstige Einkünfte.
-
-    Reference: § 9a Satz 1 Nr. 3 EStG.
-    """
-    return werbungskostenpauschbetrag
+    """Sonstige Einkünfte nach Abzug der Werbungskosten."""
+    return max(
+        rente__steuerpflichtige_einnahmen_y
+        + alle_weiteren_y
+        - werbungskostenpauschbetrag,
+        0.0,
+    )
